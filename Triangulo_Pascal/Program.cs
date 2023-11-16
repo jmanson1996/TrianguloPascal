d75acc37812d6ba0411d647df08e764d1877c0de
Jonathan Manson <jonathanmanson_1996@hotmail.com>
2023-11-16 18:06:41 -0500

    triangulo de pasal

---
+++ Triangulo_Pascal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Traingulo_Pascal
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            int pisos = 5;
+            int[] array = new int[1];
+            Console.WriteLine("ingrese el numero de pisos");
+            pisos = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < pisos; i++)
+            {
+                int[] pascal = new int[i];
+                for (int j = pisos; j < i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < i; k++)
+                {
+                    if (k == 0 || k == (i - 1))
+                    {
+                        pascal[k] = 1;
+                    }
+                    else
+                    {
+                        pascal[k] = array[k] + array[k - 1];
+                    }
+                    Console.Write("[ " + pascal[k] + "]");
+                }
+                array = pascal;
+
+                Console.WriteLine(" ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
